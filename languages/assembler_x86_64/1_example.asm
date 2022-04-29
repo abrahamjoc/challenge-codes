77455fec58067a039157d46c86000bc9b0b7448f
José Abraham <abrahamjoc@gmail.com>
2022-04-28 21:32:13 -0400

    style(asm): 1_example.asm

--- languages/assembler_x86_64/1_example.asm
+++ languages/assembler_x86_64/1_example.asm
@@ -5,25 +5,25 @@
 ; ----------------------------------------------------------------------------------------
 
 section .data
-msg:	db "Hello, world!", 0x0A, 0
-mlen:   db $-msg  ; len(msg)
+msg:	   db "Hello, world!", 0x0A, 0
+msg_len:   db $-msg  ; len(msg)
 
 section .text
     global main
 
 main:
-    push rbp      ; create new stack frame
+    push rbp         ; create new stack frame
     mov rbp, rsp
 
-    mov rax, 1    ; system call for write
-    mov rdi, 1    ; stdout
-    mov rsi, msg  ; address of string to output
-    mov rdx, mlen ; number of bytes of msg
-    syscall       ; invoke operating system to do the write
+    mov rax, 1       ; system call for write
+    mov rdi, 1       ; stdout
+    mov rsi, msg     ; address of string to output
+    mov rdx, msg_len ; number of bytes of msg
+    syscall          ; invoke operating system to do the write
 
-    mov rsp, rbp  ; restore old stack frame
+    mov rsp, rbp     ; restore old stack frame
     pop rbp
     
-    mov rax, 60   ; system call for exit
-    mov rdi, 1    ; exit code 1
-    syscall       ; invoke operating system to exit
+    mov rax, 60      ; system call for exit
+    mov rdi, 1       ; exit code 1
+    syscall          ; invoke operating system to exit
