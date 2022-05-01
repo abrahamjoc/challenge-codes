f278a730e5ae98ae18929cd652165792067aa9f7
José Abraham <abrahamjoc@gmail.com>
2022-05-01 17:23:19 -0400

    style(asm): 7_example.asm

--- languages/assembler_x86_64/7_example.asm
+++ languages/assembler_x86_64/7_example.asm
@@ -5,7 +5,8 @@
 ; ------------------------------------------------------------------------------------
 
 section .data
-    print_msg: db `You introduced in the buffer variable:\n --> %s`, 0
+    header_msg: db "Write a message:", 0x0A, 0
+    output_msg: db `You introduced in the buffer variable:\n --> %s`, 0
 
 section .bss
     buffer: resb 255    ; declare buffer in memory = buffer_string[255]
@@ -18,15 +19,18 @@ main:
     push rbp            ; create new stack frame
     mov rbp, rsp
 
+    mov rdi, header_msg ; set 1th function parameter = address of header_msg
+    call printf         ; printf(header_msg, buffer)
+
     mov rax, 0          ; system call for read
     mov rdi, 0          ; stdin
     mov rsi, buffer     ; address of string to input
     mov rdx, 255        ; number of bytes of buffer_in_string
     syscall             ; invoke operating system to do the read
 
-    mov rdi, print_msg  ; set 1th function parameter = address of print_msg
+    mov rdi, output_msg ; set 1th function parameter = address of output_msg
     mov rsi, buffer     ; set 2th function parameter = address of buffer
-    call printf         ; printf(print_msg, buffer)
+    call printf         ; printf(output_msg, buffer)
 
     leave               ; restore old stack frame
     mov	rax, 0          ; exit(0)
