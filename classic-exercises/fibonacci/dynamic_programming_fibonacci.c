b7a7ae2867f650d21dff096041c28ac212b2a4fd
José Abraham <abrahamjoc@gmail.com>
2022-08-20 10:17:10 -0400

    exercise: dynamic programming fibonacci

---
+++ classic-exercises/fibonacci/dynamic_programming_fibonacci.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+double fibonacci(int n) {
+    double *list = (double*) malloc( (n+1) * sizeof(double) );
+    list[0] = 0;
+    list[1] = 1;
+    for ( int i=2; i<=n; ++i )
+        list[i] = list[i-2]+list[i-1];
+    double r = list[n];
+    free(list);
+    return r;
+}
+
+void calcFibonacci(int n) {
+    clock_t bt = clock();
+    double r = fibonacci(n);
+    clock_t et = clock();
+    double secs = difftime(et, bt) / CLOCKS_PER_SEC;
+
+    printf("f(%d)=%.0f executed in %.0f secs.\n", n, r, secs);
+}
+
+int main(int argc, char* argv[]) {
+    if ( argc < 2) {
+        fprintf(stderr, "Usage: %s [-r] N\n", argv[0]);
+        return -1;
+    }
+
+    int opt;
+    enum { SINGLE_MODE, RANGE_MODE } mode = SINGLE_MODE;
+    while ((opt = getopt(argc, argv, "r")) != -1) {
+        switch (opt) {
+        case 'r': mode = RANGE_MODE; break;
+        default:
+            exit(1);
+        }
+    }
+    
+    int n = atoi(argv[argc-1]);
+    
+    int i = mode == RANGE_MODE ? 0 : n;
+    for ( ; i<=n; ++i )
+        calcFibonacci(i);
+
+    return 0;
+}
