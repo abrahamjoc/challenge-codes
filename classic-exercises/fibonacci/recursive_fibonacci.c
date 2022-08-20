4bb0ed05b813ec7e84e4693bbbe9a61f708a3390
José Abraham <abrahamjoc@gmail.com>
2022-08-20 03:45:37 -0400

    doc: recursive_fibonacci

---
+++ classic-exercises/fibonacci/recursive_fibonacci.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+double fibonacci(int n) {
+    if ( n == 0 ) return 0;
+    if ( n == 1 ) return 1;
+    return fibonacci(n-2)+fibonacci(n-1);
+}
+
+void calcFibonacci(int n) {
+    clock_t bt = clock();
+    double r = fibonacci(n);
+    clock_t et = clock();
+    double secs = difftime(et, bt) / CLOCKS_PER_SEC;
+    printf("f(%d)=%.0f executed in %.0f secs.\n", n, r, secs);
+}
+
+int main(int argc, char* argv[]) {
+    if ( argc < 2) {
+        fprintf(stderr, "Usage: %s [-r] N\n", argv[0]);
+        return -1;
+    }
+
+    int opt;
+    enum { SINGLE_MODE, RANGE_MODE } mode = SINGLE_MODE;
+    while ((opt = getopt(argc, argv, "r")) != -1) {
+        switch (opt) {
+        case 'r': mode = RANGE_MODE; break;
+        default:
+            exit(1);
+        }
+    }
+    
+    int n = atoi(argv[argc-1]);
+    for ( int i=(mode == RANGE_MODE ? 0:n); i<=n; ++i ) {
+        calcFibonacci(i);
+    }
+
+    return 0;
+}
