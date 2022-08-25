71c5479847ef11f09fc2cb7c6b46b4a7e2c2e923
José Abraham <abrahamjoc@gmail.com>
2022-08-24 22:15:50 -0400

    exercise: factorial

---
+++ classic-exercises/factorial/static_factorial.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+
+/*
+ *  This code explains how works factorial using recursion. 
+ *  How to run: ./static_factorial N
+ */
+
+
+/*
+ * Factorial: calculator
+ */
+double factorial(int n) {
+    if ( n == 0 ) return -1;
+    static double res = 1;
+    res = res * n;
+    factorial(n-1);
+    return res;
+}
+
+/*
+ * CalcFactorial: wrapper function to call factorial calculator and show how many time take the calculation.
+ */
+void calcFactorial(int n) {
+    clock_t bt = clock();
+    double r = factorial(n);
+    clock_t et = clock();
+
+    double secs = difftime(et, bt) / CLOCKS_PER_SEC;
+    printf("f(%d)=%.0f executed in %.0f secs.\n", n, r, secs);
+}
+
+int main(int argc, char* argv[]) {
+    if ( argc < 2 ) {
+        fprintf(stderr, "Usage: %s N\n", argv[0]);
+        return -1;
+    }
+
+    int n = atoi(argv[argc-1]);
+    
+    calcFactorial(n);
+
+    return 0;
+}
